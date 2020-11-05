9ef8764cfa70f1e9b71b93bf7ccb992f82f2227b
lindellcarternyc <lindellcarternyc@gmail.com>
2020-11-04 20:16:28 -0500

    create new fake app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const StyledApp = styled.div`
+  border: 1px solid red;
+`
 function App() {
   return (
-    <div className="App">
+    <StyledApp>
 Hello World
-    </div>
+    </StyledApp>
   );
 }
 
