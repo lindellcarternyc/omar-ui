fcb1b66543e97495da00f88d0bb1997033dcd068
lindellcarternyc <lindellcarternyc@gmail.com>
2020-11-06 18:52:00 -0500

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { Button } from './components/Button'
+import { ColorSwatch } from './components/ColorSwatch'
+
 const StyledApp = styled.div`
   border: 1px solid red;
 `
+
 function App() {
   return (
     <StyledApp>
-Hello World
+      <Button>
+        Button
+      </Button>
+      <ColorSwatch color="darkblue" />
+      <ColorSwatch color="dark blue" />
+      <ColorSwatch color="#eaeaea"/>
     </StyledApp>
   );
 }
