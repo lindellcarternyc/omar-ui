f6250af88d2e8375ee3d9e3a879429c9175bfe7a
lindellcarternyc <lindellcarternyc@gmail.com>
2020-11-06 18:50:43 -0500

    create color constants

---
+++ src/components/constants/colors.ts
@@ -0,0 +1,8 @@
+export const Red = '#ef476f'
+export const Yellow = '#ffd166'
+export const Green = '#06d6a0'
+export const MediumBlue = '#118ab2'
+export const DarkBlue = '#073b4c'
+export const MediumGray = '#aeaeae'
+export const White = '#ffffff'
+export const Black = '#353535'
